3e0d7dd431a25c8e75f3e8508801c5cc7f53ed57
blackman <lujun19950@gmail.com>
2024-06-29 17:22:49 +0800

    创建了StuController

--- foodie-mall-api/src/main/java/com/blackman/controller/StuController.java
+++ foodie-mall-api/src/main/java/com/blackman/controller/StuController.java
@@ -4,18 +4,20 @@ import com.blackman.pojo.Stu;
 import com.blackman.service.StuService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/stu")
+@RestController
+@RequestMapping("/stu")
 public class StuController {
 
     @Autowired
     private StuService stuService;
 
-    @GetMapping("/getStuInfo")
-    public Stu getStuInfo(Integer id) {
+   @GetMapping("/getStuInfo")
+   public Stu getStuInfo(@RequestParam(name="id") Integer id) {
        return stuService.getStuInfo(id);
-    }
+   }
+
 }
